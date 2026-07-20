db67a1edbab748d61a70abd70762f5124a7c2907
Tales Santos <tales.js7@gmail.com>
2026-07-20 13:24:10 -0300

    Ugprade files

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from "./app.js";
 
-const PORT = process.env.PORT || "https://bot-55rk.onrender.com";
+const PORT = process.env.PORT || 3000;
 
 // Aguarda carregar o bot somente depois do dotenv
 await import("./bot/index.js");
