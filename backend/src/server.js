491709e8e1b5202d1566856add9cffb2f43469a6
Tales Santos <tales.js7@gmail.com>
2026-07-20 23:28:51 -0300

    Upgrade files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
 
+const app = express();
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +10,12 @@ const PORT = process.env.PORT || 3000;
 // Aguarda carregar o bot somente depois do dotenv
 await import("./bot/index.js");
 
+app.get("/", (req, res) => {
+  res.send("Bot Online");
+});
+
 app.listen(PORT, () => {
   console.log(`API rodando na porta ${PORT}`);
 });
+
+bot.launch();
