a476500d217e5b37076c6e021af2190ceb935e08
Tales Santos <tales.js7@gmail.com>
2026-08-02 21:32:35 -0300

    Upgrdade files

--- backend/src/bot/commands.js
+++ backend/src/bot/commands.js
@@ -1,14 +1,11 @@
 import { bot } from "./bot.js";
 import { menuPrincipal } from "./keyboards.js";
-import { Markup, Input } from "telegraf";
 
 bot.start(async (ctx) => {
-
-  const msg = await ctx.replyWithVideo(
-    Input.fromLocalFile("BAACAgEAAxkDAAIDeWpvzvasqv-6VZkuSoUO8rEIxgjwAAI5CQACaFaAR4DG8EZZo6lIPQQ"),
+  await ctx.replyWithVideo(
+    "BAACAgEAAxkDAAIDeWpvzvasqv-6VZkuSoUO8rEIxgjwAAI5CQACaFaAR4DG8EZZo6lIPQQ",
     {
-            caption:
-`🔥 𝗕𝗘𝗠-𝗩𝗜𝗡𝗗𝗢 𝗔𝗢 𝗨𝗡𝗜𝗩𝗘𝗥𝗦𝗢 DO PRAZER 🔥
+      caption: `🔥 𝗕𝗘𝗠-𝗩𝗜𝗡𝗗𝗢 𝗔𝗢 𝗨𝗡𝗜𝗩𝗘𝗥𝗦𝗢 DO PRAZER 🔥
 🇧🇷 Faveladinhas gostosas, desesperadinhas, ninfetas de bairro, magrinhas peitudas, todo tipo de vazado bruto em um único grupo 😈🥵
 
 𝗔𝗤𝗨𝗜 É 𝗠𝗔𝗧𝗘𝗥𝗜𝗔𝗟 𝗦𝗨𝗝𝗢, 𝗦𝗘𝗠 𝗙𝗜𝗟𝗧𝗥𝗢:
@@ -17,10 +14,7 @@ Selecione o produto que deseja acessar.
 
 Basta clicar em um botão abaixo.`,
 
-            ...menuPrincipal
-        }
-
-
-    );
-
+      ...menuPrincipal,
+    },
+  );
 });
