8276dc0c123d8acb81015596fe571a9b88caa68a
Tales Santos <tales.js7@gmail.com>
2026-09-21 19:41:24 -0300

    Upgrdade files

--- backend/src/bot/products.js
+++ backend/src/bot/products.js
@@ -8,112 +8,38 @@ export const products = [
     plans: [
       {
         id: "7dias",
-        name: "7 dias",
+        name: "😈 7 dias",
         price: 5.90,
         duration: 7,
       },
       {
         id: "1mes",
-        name: "1 mês",
+        name: "🔞 1 mês",
         price: 10.90,
         duration: 30,
       },
       {
         id: "vitalicio",
-        name: "Vitalício",
+        name: "💎 Vitalício",
         price: 29.90,
         duration: null,
       },
     ],
 
     description: `
-🔞 <b>ÁREA VIP</b>
+🔞 <b>VAZADINHOS DA NET</b> 
+━━━━━━━━━━━━━━━━━━ 
+💎 <b>ÁREA VIP EXCLUSIVA</b> 
+🔒 Acesso 100% privado 
+🔥 Conteúdo exclusivo 
+📂 Tudo organizado por categorias 
+📅 Novas atualizações frequentes 
+⚡ Liberação automática após o pagamento
 
-━━━━━━━━━━━━━━━━━━
-
-💎 Acesso privado
-📂 Conteúdo organizado
-⚡ Liberação após pagamento
-
-Escolha o seu plano abaixo.
+💎 Escolha o seu plano:
 `,
   },
 
-  //   {
-  //     id: 2,
-  //     callback: "produto_1",
-  //     name: "🏳️‍⚧️ Top Trans VIP",
-  //     price: 9.90,
 
-  //     groupId: "-1004331849848",
-
-  //     description: `
-  // 🏳️‍⚧️ <b>TOP TRANS VIP</b>
-
-//      ━━━━━━━━━━━━━━━━━━
-
-//   🔒 Grupo Privado
-
-//   📁 Conteúdo organizado
-
-//   📈 Atualizações frequentes
-
-//   ⚡ Liberação automática
-
-//   ━━━━━━━━━━━━━━━━━━
-
-  // 💰 <b>Valor:</b> R$ 9,90`
-  //   },
-
-  //   {
-  //     id: 3,
-  //     callback: "produto_2",
-  //     name: "😈 OnlyFans + Privacy",
-  //     price: 15.90,
-
-  //     groupId: "-1004405156525",
-
-  //     description: `
-  // 😈 <b>ONLYFANS + PRIVACY VIP</b>
-
-  // ━━━━━━━━━━━━━━━━━━
-
-  // 🔒 Grupo Privado
-
-  // 📁 Conteúdo organizado
-
-  // 📈 Atualizações frequentes
-
-  // ⚡ Liberação automática
-
-  // ━━━━━━━━━━━━━━━━━━
-
-  // 💰 <b>Valor:</b> R$ 15,90`
-  //   },
-
-  //   {
-  //     id: 4,
-  //     callback: "produto_3",
-  //     name: "🔥 Faveladinhas VIP",
-  //     price: 69.90,
-
-  //     groupId: "-1003333333333",
-
-  //     description: `
-  // 🔥 <b>FAVELADINHAS VIP</b>
-
-  // ━━━━━━━━━━━━━━━━━━
-
-  // 🔒 Grupo Privado
-
-  // 📁 Conteúdo organizado
-
-  // 📈 Atualizações frequentes
-
-  // ⚡ Liberação automática
-
-  // ━━━━━━━━━━━━━━━━━━
-
-  // 💰 <b>Valor:</b> R$ 69,90`
-  //   }
+  
 ];
